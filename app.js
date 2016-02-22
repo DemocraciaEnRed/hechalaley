0cc4ed750fb3f456e8b85631867dd4b5e7f3c024
Jota De Picas <jotadepicas@gmail.com>
2016-02-22 01:50:58 -0300

    first steps with the Bill REST API and mongo

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-var express = require("express"),  
-    app = express(),
-    mongoose = require('mongoose');
-
-
+var express = require("express");
+var app = express();
 var router = express.Router();
+var bodyParser = require('body-parser');
 
-router.get('/', function(req, res) {  
-   res.send("Hello World!");
+var mongoose = require('mongoose');
+
+router.get('/', function(req, res) {
+	res.send("Hello World!");
 });
 
 app.use(router);
+app.use(bodyParser.json());
 
-app.listen(3000, function() {  
-  console.log("Hola viteh on http://localhost:3000");
+routes = require('./lib/bill/billRoutes')(app);
+
+var databaseURL = 'mongodb://localhost/billtracker';
+mongoose.connect(databaseURL, function(err, res) {
+	if (err) {
+		console.log('ERROR: connecting to Database. ' + err);
+	} else {
+		console.log('Connected to database ' + databaseURL);
+		var portNumber = 3000;
+		app.listen(portNumber, function() {
+			console.log("BillTracker server running on http://localhost:" + portNumber);
+		});
+	}
 });
