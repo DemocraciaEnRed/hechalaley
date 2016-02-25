d0100e50f39708c93b827a224a29524bb2bae032
Matías Lescano <matiasj.lescano@gmail.com>
2016-02-25 11:18:18 -0300

    add css build

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
 var gulp = require('gulp');
 var assets = require('./lib/build/assets');
+var css = require('./lib/build/css');
 
-gulp.task('assets', assets);
+gulp
+  .task('assets:build', assets.build)
+  .task('css:build', css.build)
+  .task('build', ['assets:build', 'css:build'])
+  .task('default', ['build']);
