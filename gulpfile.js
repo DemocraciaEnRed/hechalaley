cbff7eace70cf5f98467acb3569d3a7f9b400f07
Matías Lescano <matiasj.lescano@gmail.com>
2016-02-26 12:34:36 -0300

    add browserify + watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,1 @@
-var gulp = require('gulp');
-var assets = require('./lib/build/assets');
-var css = require('./lib/build/css');
-var js = require('./lib/build/js');
-
-gulp
-  .task('assets:build', assets.build)
-  .task('css:build', css.build)
-  .task('js:build', js.build)
-  .task('build', ['assets:build', 'css:build', 'js:build'])
-  .task('default', ['build']);
+require('./lib/build');
