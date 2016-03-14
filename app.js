10044d677c45f26c4485b93eb69178848060aa1c
Jota De Picas <jotadepicas@gmail.com>
2016-03-14 16:15:50 -0300

    compliance with standardjs.com. This commits mostly just removes semicolons.

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
-var bodyParser = require('body-parser');
-var config = require('config');
-var express = require('express');
-var log = require('debug')('billtracker:root');
-var mongoose = require('mongoose');
-var os = require('os');
+var bodyParser = require('body-parser')
+var config = require('config')
+var express = require('express')
+var log = require('debug')('billtracker:root')
+var mongoose = require('mongoose')
+var os = require('os')
 
-var app = express();
+var app = express()
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.use(function (req, res, next) {
-  log(req.method + ' ' + req.path);
-  next();
-});
+  log(req.method + ' ' + req.path)
+  next()
+})
 
-require('./lib/bill')(app);
+require('./lib/bill')(app)
 
-app.use(require('./lib/site'));
-app.use(require('./lib/admin'));
+app.use(require('./lib/site'))
+app.use(require('./lib/admin'))
 
-app.use(express.static('public'));
-app.use(express.static('build'));
+app.use(express.static('public'))
+app.use(express.static('build'))
 
 app.get('*', function (req, res) {
-  res.status(404).send('Not found!');
-});
+  res.status(404).send('Not found!')
+})
 
-var databaseURL = config.mongoUrl;
+var databaseURL = config.mongoUrl
 mongoose.connect(databaseURL, function (err, res) {
   if (err) {
-    log('ERROR: connecting to Database. ' + err);
+    log('ERROR: connecting to Database. ' + err)
   } else {
-    log('Connected to database ' + databaseURL);
-    var portNumber = config.port || 3000;
+    log('Connected to database ' + databaseURL)
+    var portNumber = config.port || 3000
     app.listen(portNumber, function () {
-      log('BillTracker server running on http://' + os.hostname() + ':' + portNumber);
-    });
+      log('BillTracker server running on http://' + os.hostname() + ':' + portNumber)
+    })
   }
-});
+})
--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 module.exports = require('democracyos-config')({
   path: __dirname
-});
+})
