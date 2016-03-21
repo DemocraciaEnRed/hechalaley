3f911cebf5083c05d51831618ceae1efa4cf3314
Matías Lescano <matiasj.lescano@gmail.com>
2016-03-21 12:06:17 -0300

    resolve absolute path of config.billsGitPath

--- config/index.js
+++ config/index.js
@@ -1,3 +1,10 @@
-module.exports = require('democracyos-config')({
+var path = require('path')
+
+var config = require('democracyos-config')({
   path: __dirname
 })
+
+// Resolve absolute path for billsGitPath
+config.billsGitPath = path.resolve(__dirname, config.billsGitPath)
+
+module.exports = config
