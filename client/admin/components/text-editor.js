d98a0f677c0bd2fc915d1a5273c9de66c1ed222b
Matías Lescano <mjlescano@protonmail.com>
2017-10-29 11:34:08 -0300

    Fix textarea styles

--- client/admin/components/text-editor.js
+++ client/admin/components/text-editor.js
@@ -78,11 +78,15 @@ export default class TextEditor extends Component {
           <p><small><strong>Markdown</strong> es el formato base que se utiliza para guardar el texto en la base de datos. Aquí puedes editarlo directamente, o copiar y pegarlo desde otras fuentes sin arruinar los estilos.</small></p>
           <Textarea
             style={{
+              boxSizing: 'border-box',
               display: 'block',
               width: '100%',
               fontFamily: 'monospace',
-              fontSize: '14px',
-              resize: 'vertical'
+              fontSize: '1.1rem',
+              lineHeight: 1.6,
+              resize: 'vertical',
+              padding: '1rem',
+              border: 0
             }}
             value={this.state.value}
             onChange={this.handleTextChange} />
