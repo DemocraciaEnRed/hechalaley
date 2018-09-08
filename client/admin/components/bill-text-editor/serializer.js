adf24733371df13a45e310dce595c41b3c79fd07
Matías Lescano <mjlescano@protonmail.com>
2018-09-08 20:05:46 -0300

    Update files organization

--- client/admin/components/bill-text-editor/serializer.js
+++ client/admin/components/bill-text-editor/serializer.js
@@ -2,8 +2,4 @@ import MarkdownSerializer from 'slate-md-serializer'
 
 const Markdown = new MarkdownSerializer()
 
-export const deserialize = (md) => {
-  const value = Markdown.deserialize(md)
-  console.log(value)
-  return value
-}
+export const deserialize = (md) => Markdown.deserialize(md)
