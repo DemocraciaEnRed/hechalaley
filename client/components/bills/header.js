6fa1c1fc2c283570edf5d001657f7cfb8e26ada4
Matías Lescano <mjlescano@protonmail.com>
2018-11-08 01:52:36 -0300

    Add menu example

--- client/components/bills/header.js
+++ client/components/bills/header.js
@@ -1,3 +1,5 @@
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs'
+
 const Header = ({ title }) => (
   <div className='header'>
     <style>
@@ -12,9 +14,42 @@ const Header = ({ title }) => (
           margin-bottom: 1em;
           hyphens: auto;
         }
+
+        .submenu {
+          margin-bottom: 3em;
+        }
       `}
     </style>
     <h1 className='bills-content'>{title}</h1>
+    <Tabs className='submenu bills-content'>
+      <TabList>
+        <Tab>Información General</Tab>
+        <Tab>Descripción</Tab>
+        <Tab>Votantes</Tab>
+      </TabList>
+      <TabPanel>
+        <p>
+          Ex pariatur et in ut aut veniam. Quia autem praesentium laboriosam quo
+          quis. Non adipisci non enim distinctio nihil dolores assumenda
+          consectetur.
+        </p>
+      </TabPanel>
+      <TabPanel>
+        <p>
+          Vero aut animi at est rerum libero consequatur. Accusantium eius est
+          similique et consequuntur animi quos eos.
+        </p>
+      </TabPanel>
+      <TabPanel>
+        <p>
+        Fugiat quae atque qui officiis. Non temporibus est dolores delectus
+        blanditiis est hic. Qui doloribus sint rem optio unde. Quibusdam omnis
+        harum itaque ab possimus minus quo fugiat. Delectus sint autem ipsa non
+        dignissimos eos qui. Est odio sed doloremque expedita nulla officia
+        dolores et.
+        </p>
+      </TabPanel>
+    </Tabs>
   </div>
 )
 
