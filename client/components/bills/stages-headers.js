7335c243d096bbca8e177ec163635b90db8e9305
juan pernumian <juan.pernumian@mercadolibre.com>
2018-11-13 11:10:45 -0300

    Style header on compare

--- client/components/bills/stages-headers.js
+++ client/components/bills/stages-headers.js
@@ -12,6 +12,21 @@ const StageHeader = ({ stage }) => {
             margin-bottom: 3rem;
           }
 
+          .stage-header :global(.bill-tab) {
+            display: inline-block;
+            padding: 0 50px;
+            color: #9196a4;
+          }
+
+          .stage-header :global(.active) {
+            color: #fe3e68;
+          }
+
+          .stage-header :global(.bill-tablist) {
+            padding-bottom: 32px;
+            border-bottom: 1px solid #e0e0e0;
+          }
+
           .submenu {
             margin-bottom: 3em;
           }
@@ -19,20 +34,48 @@ const StageHeader = ({ stage }) => {
           .stage-header :global(.active) {
             font-weight: bold;
           }
+
+          .stage-data {
+            display: inline-block;
+            margin: 0 15px;
+          }
+
+          .stage-data::before {
+            content: ' ';
+            widht: 2px;
+            background-color: grey;
+          }
+
+          .stage-data-details {
+            display: block;
+          }
+
+          .stage-header :global(.stage-data-wrapper) {
+            text-align: left;
+          }
         `}
       </style>
       <Tabs selectedTabClassName='active' className='submenu'>
-        <TabList>
-          <Tab>Información General</Tab>
-          <Tab>Descripción</Tab>
-          <Tab>Votantes</Tab>
+        <TabList className='bill-tablist'>
+          <Tab className='bill-tab'>Información General</Tab>
+          <Tab className='bill-tab'>Descripción</Tab>
+          <Tab className='bill-tab'>Votantes</Tab>
         </TabList>
         <hr />
-        <TabPanel>
+        <TabPanel className='stage-data-wrapper'>
           <ul>
-            <li>Fecha {printDate(stage.stageDate)}</li>
-            <li>Estado Rechazado</li>
-            <li>Próximos Pasos: Sin definir</li>
+            <li className='stage-data'>
+              Fecha
+              <span className='stage-data-details'>{printDate(stage.stageDate)}</span>
+            </li>
+            <li className='stage-data'>
+              Estado
+              <span className='stage-data-details'>Rechazado</span>
+            </li>
+            <li className='stage-data'>
+              Próximos Pasos
+              <span className='stage-data-details'>Sin definir</span>
+            </li>
           </ul>
         </TabPanel>
         <TabPanel>
